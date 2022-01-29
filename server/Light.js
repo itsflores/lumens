26ddfc4b1d173f8942c316d67ca3035276a6699f
Omar Flores <omar.garciaf247@gmail.com>
2022-01-29 13:32:48 -0500

    updates to light class

--- server/Light.js
+++ server/Light.js
@@ -1,14 +1,26 @@
 class Light {
   intensity;
   on;
+  x;
+  y;
+  z;
+  radius;
 
-  constructor() {
+  constructor(x, y, z, radius = 10) {
     this.intensity = 0;
     this.on = false;
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.radius = radius;
   }
 
   setIntensity(intensity) {
     this.setIntensity = intensity;
+  }
+
+  setRadius(radius) {
+    this.radius = radius;
   }
 
   switch() {
