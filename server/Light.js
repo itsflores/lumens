679903d34ca66e146352d79e33be659d3d34370d
Omar Flores <omar.garciaf247@gmail.com>
2022-02-03 10:36:00 -0500

    updates to light class

--- server/Light.js
+++ server/Light.js
@@ -1,18 +1,25 @@
 class Light {
   intensity;
   on;
+
   x;
   y;
   z;
+
   radius;
 
-  constructor(x, y, z, radius = 10) {
+  note;
+  octave;
+
+  constructor(x, y, z, radius = 10, note, octave) {
     this.intensity = 0;
     this.on = false;
     this.x = x;
     this.y = y;
     this.z = z;
     this.radius = radius;
+    this.note = note;
+    this.octave = octave;
   }
 
   setIntensity(intensity) {
@@ -25,5 +32,9 @@ class Light {
 
   switch() {
     this.on = !this.on;
+  }
+
+  updateIntensity(x, y, z) {
+    // calculations
   }
 }
